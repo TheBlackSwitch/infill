e3991b962f69763f10fd0886a37de2c7f955111c
TheBlackSwitch <115941826+TheBlackSwitch@users.noreply.github.com>
2026-09-04 20:04:05 +0200

    update yamp + logo

--- src/workers/parse.ts
+++
@@ -1,9 +0,0 @@
-import * as YAMP from "@theblackswitch/yamp"
-
-// Look at this puny tiny bit of code
-
-self.onmessage = (e) => {
-    const result = YAMP.parse(e.data);
-    console.log(result);
-    self.postMessage(result);
-}
